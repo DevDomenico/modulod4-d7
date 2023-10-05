e79ae9d72f710724e7be48b41dbe5d9552fd3934
Domenico <135279072+DevDomenico@users.noreply.github.com>
2023-10-05 19:02:03 +0200

    esercizio

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,64 @@
 
 let lista = [];
+const url = 'https://striveschool-api.herokuapp.com/api/product/'
 async function fetchLocalJson() {
-    try {
-        const response = await fetch('lista.json');
-        if (!response.ok) {
-            throw new Error('Errore, lista non trovata');
-        }
+
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: {
+                Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTFjNzFjNmJmNGJlNzAwMThiMWIyMGIiLCJpYXQiOjE2OTY0NDY5NjgsImV4cCI6MTY5NzY1NjU2OH0.po06F_J-Wn1dxcLvY2nmuB9opBnd51xxXCJ0P6XNOic'
+            }
+        })
         const data = await response.json();
         lista = data;
         stampa();
-    } catch (error) {
-        console.error('Si è verificato un errore:', error);
-    }
+    
 }
- const htmlstampa= document.querySelector('#lista');
-const  stampa=() =>{ 
+const htmlstampa = document.querySelector('#lista');
+const stampa = () => {
     console.log(lista);
     lista.forEach(prodotto => {
         const card = document.createElement('div');
         card.className = 'card';
         card.innerHTML = `
-        <img src="${prodotto.immagine}" alt="${prodotto.nome}">
-        <h3>${prodotto.nome}</h3>
+        <img src="${prodotto.imageUrl}" alt="${prodotto.name}">
+        <h3>${prodotto.name}</h3>
         <button  class=' btn btn-success' type="button"> dettagli </button>
-        <p>Prezzo: $${prodotto.prezzo.toFixed(2)}</p>
+        <p>Prezzo: $${prodotto.price}</p>
             `;
-            htmlstampa.appendChild(card);
+        htmlstampa.appendChild(card);
+
+     //posta(prodotto) 
     });
+   
+
 }
+/* const  posta= async (prodotto) => {
+    const response = await fetch("https://striveschool-api.herokuapp.com/api/product/", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTFjNzFjNmJmNGJlNzAwMThiMWIyMGIiLCJpYXQiOjE2OTY0MjkzMjYsImV4cCI6MTY5NzYzODkyNn0.x1f_FzKOEAmIBc3hSPIMaxLwc46r2F1VrWfFDnWUlLo"
+        },
+        body: JSON.stringify({
+            name: prodotto.nome,
+            description: prodotto.descrizione,
+            price: prodotto.prezzo,
+            brand: prodotto.brand,
+           imageUrl: prodotto.immagine
+      
+        })
+    })
+    
+    if (response.ok) { 
+        console.log("funzione")
+     
+    } else {
+        console.error("errore non funziona")
+    }
+            
+    
+}*/
 
 window.onload = () => fetchLocalJson();
+
