faf2a0bfadf71bad61efec6963695647fd1d38ff
Domenico <135279072+DevDomenico@users.noreply.github.com>
2023-10-28 22:36:15 +0200

    esercizio

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,16 +3,16 @@ let lista = [];
 const url = 'https://striveschool-api.herokuapp.com/api/product/'
 async function fetchLocalJson() {
 
-        const response = await fetch(url, {
-            method: 'GET',
-            headers: {
-                Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTFjNzFjNmJmNGJlNzAwMThiMWIyMGIiLCJpYXQiOjE2OTY0NDY5NjgsImV4cCI6MTY5NzY1NjU2OH0.po06F_J-Wn1dxcLvY2nmuB9opBnd51xxXCJ0P6XNOic'
-            }
-        })
-        const data = await response.json();
-        lista = data;
-        stampa();
-    
+    const response = await fetch(url, {
+        method: 'GET',
+        headers: {
+            Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTNkNjMwZTYzZjE1ODAwMTQxMTg0ODUiLCJpYXQiOjE2OTg1MjE4NzAsImV4cCI6MTY5OTczMTQ3MH0.jRiqIJNSmduYcecsZB3WmflohnHIag1tTidLgPB0OwI'
+        }
+    })
+    const data = await response.json();
+    lista = data;
+    stampa();
+
 }
 // Funzione per centrare le immagini
 const centerImages = () => {
@@ -30,7 +30,7 @@ const stampa = () => {
         card.className = 'card';
         card.innerHTML = `
         <div class="image-container">
-            <img src="${prodotto.imageUrl}" alt="${prodotto.name}" class='small-image'>
+            <img src="${prodotto.imageUrl}" alt="${prodotto.name}"  class='small-image'>
         </div>
         <h3>${prodotto.name}</h3>
         <p>Prezzo: $${prodotto.price}</p>
@@ -39,56 +39,59 @@ const stampa = () => {
         `;
         htmlstampa.appendChild(card);
     });
-   
-
-}
-
-const dettaglio = (prodotto) =>{ 
-
-  
-    window.location.href = '/product.html'+'?id='+prodotto
 
 
 }
-/* const  posta= async (prodotto) => {
+
+const dettaglio = (prodotto) => {
+
+
+    window.location.href = '/product.html' + '?id=' + prodotto
+
+
+}
+
+
+window.onload = () => fetchLocalJson();
+//----------------------------------------------------------------
+
+
+/*const posta = async (prodotto) => {
     const response = await fetch("https://striveschool-api.herokuapp.com/api/product/", {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
-            "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTFjNzFjNmJmNGJlNzAwMThiMWIyMGIiLCJpYXQiOjE2OTY0MjkzMjYsImV4cCI6MTY5NzYzODkyNn0.x1f_FzKOEAmIBc3hSPIMaxLwc46r2F1VrWfFDnWUlLo"
+            "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTNkNjMwZTYzZjE1ODAwMTQxMTg0ODUiLCJpYXQiOjE2OTg1MjE4NzAsImV4cCI6MTY5OTczMTQ3MH0.jRiqIJNSmduYcecsZB3WmflohnHIag1tTidLgPB0OwI"
         },
         body: JSON.stringify({
             name: prodotto.nome,
             description: prodotto.descrizione,
             price: prodotto.prezzo,
             brand: prodotto.brand,
-           imageUrl: prodotto.immagine
-      
+            imageUrl: prodotto.immagine
+
         })
     })
-    
-    if (response.ok) { 
+
+    if (response.ok) {
         console.log("funzione")
-     
+
     } else {
         console.error("errore non funziona")
     }
-            
-    
-}*/
-
-window.onload = () => fetchLocalJson();
 
 
+}
 
+fetch('assets/lista.json', {
+    method: 'GET',
+    headers: {
+        'Accept': 'application/json',
+    },
+})
+    .then(response => response.json())
+    .then(response => response.forEach((prodotto) =>posta(prodotto)))
+   
+    ; 
 
-
-
-
-
-
-;
-
-
-
-
+*/
